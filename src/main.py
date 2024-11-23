5c608798e681d2383da1650f68fa4a388d358cf3
Mihai Ocnaru <mihai.ocnaru@mready.net>
2024-11-23 13:17:58 +0200

    added prediction for a folder

--- src/main.py
+++ src/main.py
@@ -1,11 +1,32 @@
 from testing_pipeline import TestingPipeline
+from game_model import GameModel
+from data_loader import DataLoader
+from cnn.cnn_model import CNNModel
+from util import format_path
 import os
 
 
-if __name__ == "__main__":
-    # turn off tensorflow warnings
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+def predict():
+    model = CNNModel()
+    model.load("../models/cnn_model")
 
+    for game_number in range(1, 5):
+        moves_path = f"../data/train/game_{game_number}"
+        scores_path = f"../data/train/game_{game_number}/{game_number}_scores.txt"
+        turns_path = f"../data/train/game_{game_number}/{game_number}_turns.txt"
+
+        moves = DataLoader(moves_path).load_moves()
+        game_model = GameModel(moves, turns_path, scores_path)
+
+        pipeline = TestingPipeline(moves_path, scores_path, turns_path)
+        scores = pipeline.play_game(game_model, model, game_number)
+
+        with open(format_path(f"../data/output/game_{game_number}/{game_number}_scores.txt"), "w+") as file:
+            for score in scores:
+                file.write(f"Player{str(score[0])} {str(score[1])} {str(score[2])}\n")
+
+
+def test():
     predictions = []
     for game_number in range(1, 5):
         moves_path = f"../data/train/game_{game_number}"
@@ -23,8 +44,16 @@ if __name__ == "__main__":
     turns_path = f"../data/validation/1_turns.txt"
 
     pipeline = TestingPipeline(moves_path, scores_path, turns_path)
-    predicition = pipeline.test_game(debug=True, title="Validation")
+    prediction = pipeline.test_game(debug=True, title="Validation")
     # prediction = pipeline.test_game_parallel(title="Validation")
     predictions.append(prediction)
 
     print(f"Average prediction: {sum(predictions)/len(predictions)*100:.2f}%")
+
+
+if __name__ == "__main__":
+    # turn off tensorflow warnings
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+    # test()
+    predict()
--- src/testing_pipeline.py
+++ src/testing_pipeline.py
@@ -1,5 +1,6 @@
 from data_loader import DataLoader
-from game_model import GameModel
+from game_model import GameModel, GameMove
+from game import Game
 from image_processing import ImageProcessing
 from util import *
 from tqdm import tqdm
@@ -184,8 +185,73 @@ class TestingPipeline:
 
         print(f"Predicted: {predicted}/{len(self.game.moves)-1} ({predicted/(len(self.game.moves)-1)*100:.2f}%)")
         return predicted / (len(self.game.moves)-1)
+    
 
+    def play_game(self, game_model, model, game_number=1):
+        game = Game()
+        moves = game_model.moves
 
+        if not os.path.exists(format_path(f"../data/output/game_{game_number}")):
+            os.makedirs(format_path(f"../data/output/game_{game_number}"))
+
+        for i in tqdm(range(1, len(moves)), desc=f"Playing game {game_number}"):
+            board_contor_1 = self.image_processing._find_board_contour(self.game.moves[i-1].image_path)
+            board_contor_2 = self.image_processing._find_board_contour(self.game.moves[i].image_path)
+
+            board_1 = self.image_processing.crop_board(self.game.moves[i-1].image_path, board_contor_1)
+            board_2 = self.image_processing.crop_board(self.game.moves[i].image_path, board_contor_2)
+
+            diff = self.image_processing.find_difference_between_images(board_1, board_2)
+            
+            # get original dimensions before any resizing
+            original_height, original_width = diff.shape[:2]
+
+            x, y = self.image_processing.find_added_piece_coordinates(diff)
+            w = 105
+            h = 105
+            
+            # scale coordinates for board_2
+            scale_x = board_2.shape[1] / original_width
+            scale_y = board_2.shape[0] / original_height
+            
+            board_contour = [
+                int(x * scale_x), 
+                int(y * scale_y), 
+                int((x + w) * scale_x), 
+                int((y + h) * scale_y)
+            ]
+            
+            # computing the grid rectangles
+            board_2, grid_rectangles = self.image_processing.split_board_in_blocks(board_2)
+            
+            # find the matching block
+            matching_block_idx, overlap_percentage = self.__find_matching_block(board_contour, grid_rectangles)
+            
+            if matching_block_idx != -1:
+                predicted_move = self.__convert_index_to_coordinates(matching_block_idx)
+
+                added_piece = board_2[board_contour[1]:board_contour[3], board_contour[0]:board_contour[2]]
+                added_piece = cv.resize(added_piece, (105, 105))
+                predicted_value = model.predict(added_piece)
+
+                if i < 10:
+                    index = f"0{i}"
+                else:
+                    index = str(i)
+                with open(format_path(f"../data/output/game_{game_number}/{game_number}_{index}.txt"), "w") as file:
+                    file.write(f"{predicted_move} {predicted_value}\n")
+
+                game.play(predicted_move, predicted_value)
+
+                for turn in game_model.game_turns:
+                    if int(turn.starting_position) == game.current_turn:
+                        game.change_turn()
+                        break
+
+        game.change_turn()
+        return game.scores
+                    
+    
     def __calculate_intersection_area(self, rect1, rect2):
         x_left = max(rect1[0], rect2[0])
         y_top = max(rect1[1], rect2[1])
@@ -235,6 +301,30 @@ if __name__ == "__main__":
     turns_path = f"../data/train/game_{game_number}/{game_number}_turns.txt"
 
     pipeline = TestingPipeline(moves_path, scores_path, turns_path)
-    pipeline.test_game(debug=True)
+    # pipeline.test_game(debug=True)
+    moves = DataLoader(moves_path).load_moves()
+    game_model = GameModel(moves, turns_path, scores_path)
+
+    model = CNNModel()
+    model.load("../models/cnn_model")
+
+    scores = pipeline.play_game(game_model, model)
+    # with open(format_path(f"../data/output/{game_number}_scores.txt"), "w") as file:
+    #     for score in scores:
+    #         file.write(f"Player{str(score[0])} {str(score[1])} {str(score[2])}\n")
+
+
+    files = os.listdir(format_path("../data/output"))
+    outputs = []
+    for file in files:
+        if file.endswith(".txt"):
+            with open(format_path(f"../data/output/{file}"), "r") as f:
+                outputs.append(tuple(f.read().strip().split()))
+
+    outputs = set(outputs)
+    moves = set([(move.move, str(move.value)) for move in moves[1:]])
+    print()
+    print(f"Diff: {outputs.difference(moves)}")
+
     # pipeline.test_game_parallel()
 
